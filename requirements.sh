57f5f4c7141c89401d86e2724c8a4a4afd2a9c66
codeceeker <102896343+codeceeker@users.noreply.github.com>
2022-05-25 12:22:36 +0530

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+winget install Python.Python.3
+
+# Make sure there is pip installed
+
+# Beautifulsoup. For Creating html tree and parsing
+pip install beautifulsoup4
+
+# For creating website ui using python
+pip install streamlit
+
+# Requests. For accessing website.
+pip install requests
+
